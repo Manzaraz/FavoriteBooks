8720a4e4c5396e771d00b9a6e8cb059c364d7202
Christian Manzaraz <magnusmanz@gmail.com>
2022-12-27 12:30:22 -0300

    Create a Static Table View

---
+++ FavoriteBooks/BookFormTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BookFormTableViewController.swift
+//  FavoriteBooks
+//
+//  Created by Christian Manzaraz on 27/12/2022.
+//
+
+import UIKit
+
+class BookFormTableViewController: UITableViewController {
+    
+
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var authorTextField: UITextField!
+    @IBOutlet var genreTextField: UITextField!
+    @IBOutlet var lengthTextField: UITextField!
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+
+
+}
